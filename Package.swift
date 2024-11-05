c930ed4f130445c4789dd7b1eb622cbdbdc977d4
Alex Zenla <alex@edera.dev>
2024-11-05 00:03:18 -0500

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "darwin-apk",
+    targets: [
+        .executableTarget(
+            name: "darwin-apk"),
+    ]
+)
